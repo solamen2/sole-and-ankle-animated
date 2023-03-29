0acf50b16425019009003f086ed70c43922fe15b
Edward Younskevicius <eyounskevicius@fullsail.com>
2023-03-29 13:48:56 -0700

    Implement Exercise 3: Modal enter animation, and also do Bezier curve for drawer slide-in and also stagger the fade-in for links

--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -42,9 +42,29 @@ const Overlay = styled(DialogOverlay)`
   left: 0;
   right: 0;
   bottom: 0;
-  background: var(--color-backdrop);
   display: flex;
   justify-content: flex-end;
+  background: var(--color-backdrop);
+
+  @keyframes fade-in {
+    from {
+      background: var(--color-black);
+    }
+    to {
+      background: var(--color-backdrop);
+    }
+  }
+
+  animation: fade-in 500ms cubic-bezier(.57,0,.43,1);
+
+  @keyframes fade-in-link {
+    from {
+      opacity: 0%;
+    }
+    to {
+      opacity: 100%;
+    }
+  }
 `;
 
 const Content = styled(DialogContent)`
@@ -54,6 +74,19 @@ const Content = styled(DialogContent)`
   padding: 24px 32px;
   display: flex;
   flex-direction: column;
+
+  @keyframes slide-in {
+    from {
+      transform: translateX(100%);
+    }
+    to {
+      transform: translateX(0%);
+    }
+  }
+
+  @media (prefers-reduced-motion: no-preference) {
+    animation: slide-in 500ms cubic-bezier(.57,0,.43,1);
+  }
 `;
 
 const CloseButton = styled(UnstyledButton)`
@@ -63,12 +96,6 @@ const CloseButton = styled(UnstyledButton)`
   padding: 16px;
 `;
 
-const Nav = styled.nav`
-  display: flex;
-  flex-direction: column;
-  gap: 16px;
-`;
-
 const NavLink = styled.a`
   color: var(--color-gray-900);
   font-weight: ${WEIGHTS.medium};
@@ -76,9 +103,25 @@ const NavLink = styled.a`
   font-size: 1.125rem;
   text-transform: uppercase;
 
+  :nth-child(1) { --nth-child: 1 }
+  :nth-child(2) { --nth-child: 2 }
+  :nth-child(3) { --nth-child: 3 }
+  :nth-child(4) { --nth-child: 4 }
+  :nth-child(5) { --nth-child: 5 }
+  :nth-child(6) { --nth-child: 6 }
+
   &:first-of-type {
     color: var(--color-secondary);
   }
+
+  animation: fade-in-link 500ms backwards;
+  animation-delay: calc(var(--nth-child) * 75ms);
+`;
+
+const Nav = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 16px;
 `;
 
 const Filler = styled.div`
@@ -96,6 +139,14 @@ const SubLink = styled.a`
   color: var(--color-gray-700);
   font-size: 0.875rem;
   text-decoration: none;
+
+  :nth-child(1) { --nth-child: 7 }
+  :nth-child(2) { --nth-child: 8 }
+  :nth-child(3) { --nth-child: 9 }
+
+  animation: fade-in-link 500ms backwards;
+  animation-delay: calc(var(--nth-child) * 100ms);
+  
 `;
 
 export default MobileMenu;
